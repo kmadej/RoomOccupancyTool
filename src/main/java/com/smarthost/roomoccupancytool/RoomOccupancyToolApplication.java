a0130af58aa7cb492c81afb9a8dae8117f6a6b8c
kmadej <madej.kamil91@gmail.com>
2023-11-20 17:16:38 +0100

    Initial commit using Spring Boot for RoomOccupancyToolApplication

---
+++ src/main/java/com/smarthost/roomoccupancytool/RoomOccupancyToolApplication.java
@@ -0,0 +1,13 @@
+package com.smarthost.roomoccupancytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomOccupancyToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomOccupancyToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smarthost/roomoccupancytool/RoomOccupancyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarthost.roomoccupancytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomOccupancyToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
